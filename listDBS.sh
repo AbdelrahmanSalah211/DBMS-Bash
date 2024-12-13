e798e084d5693e5e4a90cdf424f44535752597f2
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2024-12-14 00:07:31 +0200

    remove: source main_menu.sh line to avoid recursive calls

--- listDBS.sh
+++ listDBS.sh
@@ -3,11 +3,11 @@
 database="./databases"
 if [ ! -d "$database"  ] || [ -z "$(ls -A "$database")" ];then
 echo "No databases created yet"
-    source main_menu.sh
+
 
 else 
     echo "The existing databases are: "
     ls "$database"
     echo " " 
-    source main_menu.sh
+
 fi
