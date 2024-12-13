6a1e8a50ef8ca83a205ea2d71a561ec67277e859
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2024-12-14 00:01:03 +0200

    feat: list existing tables

---
+++ listTables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if compgen -G "databases/$1/*.txt" > /dev/null; then
+    echo "Tables in database $1:"
+    for file in databases/$1/*.txt; do
+        echo "$(basename "$file" ".txt")"
+    done    
+else
+    echo "No tables found for database $1"
+fi
+
