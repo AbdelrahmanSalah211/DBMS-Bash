cf68b000ccb0dc811b913d6c4485b182386c7eb7
Omar Muhammad <cds.omarmuhammad92694@alexu.edu.eg>
2024-12-20 10:36:41 +0200

    delete and update by PK and add some validation for both

--- updateTable.sh
+++ updateTable.sh
@@ -23,19 +23,74 @@ while $flag; do
     tableExit=$(grep "${tableName}|" "${metaDataFile}")
 
     if [ -n "${tableExit}" ]; then
-        primaryKeyField=$(grep "${tableName}|" "${metaDataFile}" | cut -d "|" -f 2 | cut -d "," -f 1 | cut -d ":" -f 1)
-        primaryKeyDataType=$(grep "^${tableName}|" "${metaDataFile}" | cut -d "|" -f 2 | cut -d "," -f 1 | cut -d ":" -f 2)
+        primaryKeyField=$(grep "^${tableName}|" "$metaDataFile" | awk -F"|" '{split($3,pk,":"); print pk[2]}')
+
+
+primaryKeyDataType=$(grep "^${tableName}|" "$metaDataFile" | awk -F"|" '{
+        fields=$2;
+        split($3,pk,":");
+        pk_name=pk[2];
+        
+        split(fields,field_array,",");
+        
+        # Look for the field matching PK name
+        for(i in field_array) {
+            split(field_array[i],field,":");
+            if(field[1] == pk_name) {
+                print field[2];
+                exit;
+            }
+        }
+    }')
+
+ primaryKeyPosition=$(echo "$tableExit" | awk -F"|" '{
+        fields=$2;
+        split($3,pk,":");
+        pk_name=pk[2];
+        split(fields,field_array,",");primaryKeyPosition
+        for (i in field_array) {
+            split(field_array[i],field,":");
+            if (field[1] == pk_name) {
+                print i; # Output the position (1-based index)
+                exit;
+            }
+        }
+    }')
+
         read -p "Please enter the primary key with value {$primaryKeyField} and data type {$primaryKeyDataType}: " valueRow
-        foundRow=$(awk -F ":" -v key="$valueRow" '$1 == key { print $0 }' "$dataFile")
+        foundRow=$(awk -F ":" -v key=$valueRow -v pkPos=$primaryKeyPosition '{
+        split($0,fields,":");
+        if (fields[pkPos] == key) {
+            print $0;
+        }
+    }' $dataFile)
 
         if [ -n "$foundRow" ]; then
 
-        foundNR=$(awk -F ":" -v key="$valueRow" '$1 == key { print NR }' "$dataFile")
-        foundNF=$(awk -F ":" -v key="$valueRow" '$1 == key { print NF }' "$dataFile")
+        foundNR=$(awk -F ":" -v key="$valueRow" -v key=$valueRow -v pkPos=$primaryKeyPosition '{
+        split($0,fields,":");
+        if (fields[pkPos] == key) {
+            print NR;
+        }
+    }' $dataFile)
 
 
-    IFS=',' read  -a attributeType <<< "$tableExit"
 
+
+
+        foundNF=$(awk -F ":" -v key="$valueRow" -v key=$valueRow -v pkPos=$primaryKeyPosition '{
+        split($0,fields,":");
+        if (fields[pkPos] == key) {
+            print NF;
+        }
+    }' $dataFile)
+
+    IFS='|' read -a metaDataFields <<< "$tableExit"
+
+    attributes="${metaDataFields[1]}"
+
+    IFS=',' read -a attributeType <<< "$attributes"
+    
     chosenAttributes=()
     newValues=()
             while true; do
@@ -43,13 +98,36 @@ while $flag; do
                 
                 select attr in "${attributeType[@]}" "Done"; do
                     if [ "$attr" == "Done" ]; then
-                        break 2 # Exit both the select and the outer loop
+                        break 2 
                     elif [[ -n "$attr" ]]; then
                         
                         attributeName=$(echo "$attr" | cut -d ":" -f 1)
-                        
+                        attributeDataType=$(echo "$attr" | cut -d ":" -f 2)        
                         
                         read -p "Enter the new value for $attributeName: " newValue
+
+
+                            case "$attributeDataType" in 
+                            int)
+                                if ! [[ $newValue =~ ^[0-9]+$ ]]; then
+                                    echo "invalid input. please enter an integer value."
+                                    continue
+                                fi
+                                ;;
+                            string)
+                                if ! [[ $newValue =~ ^[a-zA-Z]+$ ]]; then
+                                    echo "invalid input. please enter a string (letters only)."
+                                    continue
+                                fi
+                                ;;
+                            *)
+                                echo "Unsupported data type: $primaryKeyDataType"
+                                continue
+                                ;;
+                        esac
+
+
+
                         
                         
                         chosenAttributes+=("$attributeName")
